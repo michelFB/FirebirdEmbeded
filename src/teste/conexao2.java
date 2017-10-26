d4bdc9c526439c9df842b7ae1a78d46b96e5b8c3
michelb <michelb@dsobdrobmichelb.redechesf.local>
2017-10-26 13:49:38 -0300

    ok

---
+++ src/teste/conexao2.java
@@ -0,0 +1,18 @@
+package teste;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class conexao2 {
+	
+	
+
+public static void main(String[] args) throws SQLException, ClassNotFoundException {
+	// TODO Auto-generated method stub
+	Class.forName("org.firebirdsql.jdbc.FBDriver");  
+	String url = "jdbc:firebirdsql:embedded:c:/Users/michelb/git/novoagro/WebContent/WEB-INF/LOGIN.FDB";  
+	Connection conexao  = DriverManager.getConnection(url,"SYSDBA","masterkey"); 
+	System.out.println("Conectado!");
+	conexao.close();
+}}
